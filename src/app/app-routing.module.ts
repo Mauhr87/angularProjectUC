37fa6ad279bf24fbe043c0104217dc0822ada040
Mauricio Hidalgo <mhidalgodg@gmail.com>
2022-06-26 18:56:38 -0600

    adding routing and some components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { GifsComponent } from './gifs/gifs.component';
+import { StickersComponent } from './stickers/stickers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "home", component:HomeComponent},
+  {path: "gifs", component:GifsComponent},
+  {path: "stickers", component:StickersComponent},
+  {path: "", component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { GifsComponent } from './gifs/gifs.component';
+import { StickersComponent } from './stickers/stickers.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GifsComponent,
+    StickersComponent,
+    NavbarComponent,
+    FooterComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
